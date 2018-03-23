79771b3d9b76ee273745a0caacde8215dbc7a68e
Raul Reynoso <reynoso@Rauls-MacBook-Pro.local>
2018-03-23 13:40:13 -0500

    Make handler a generic http listener

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = (req, res) => {
+  console.log('APP')
   var data = {};
   Object.keys(req.headers).forEach(function(key){
     data[key] = req.headers[key]
   })
-  return data;
+  res.end(JSON.stringify(data)) ;
 }
